708a548f8da7f15ab0747b54fd8f5b91018a6186
jeferbc <jeffe.bernal@gmail.com>
2017-10-05 15:49:32 -0500

    characters counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.updateText = this.updateText.bind(this)
+    this.state = {
+      text: ""
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.text} onChange={this.updateText}></textarea>
+        <div className="counter">{this.state.text.length}</div>
       </div>
     );
+  }
+
+  updateText(event){
+    this.setState({
+      text: event.target.value
+    })
   }
 }
 
